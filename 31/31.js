cafc9872e41bc4ba6722d18adb4ca1e4beb1e30a
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 19:01:35 +0500

    Question 31
    ty

---
+++ 31/31.js
@@ -0,0 +1,24 @@
+"use strict";
+// Array of usernames
+let usernames = ['admin', 'Hamza', 'Usman', 'Ayesha', 'Areeba'];
+// Check if the list of users is not empty
+if (usernames.length > 0) {
+    // Loop through the array and print greetings
+    for (const username of usernames) {
+        if (username === 'admin') {
+            console.log(`Hello ${username},, would you like to see a status report?`);
+        }
+        else {
+            console.log(`Hello ${username}, thank you for logging in again.`);
+        }
+    }
+}
+else {
+    console.log("We need to find some users!");
+}
+// Remove all usernames from the array
+usernames = [];
+// Check list of users is empty after removal
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+}
