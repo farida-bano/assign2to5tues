7fac82da4f5e72647747fb3b1d7e7800c09d9644
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 18:51:12 +0500

    Question 22
    ts

---
+++ 22/22.js
@@ -0,0 +1,6 @@
+"use strict";
+let numbers = [1, 2, 3, 4, 5];
+//Error!!  Access an element at an invalid index
+console.log(numbers[10]);
+// Correctly access an element within the  array
+console.log(numbers[0]);
---
+++ 22/22.ts
@@ -0,0 +1,8 @@
+let numbers: number[] = [1, 2, 3, 4, 5];
+
+//Error!!  Access an element at an invalid index
+console.log(numbers[10]);
+
+
+// Correctly access an element within the  array
+console.log(numbers[0]);
