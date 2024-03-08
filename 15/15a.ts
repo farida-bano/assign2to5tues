cdbda62a012f82d73f8f53c6e604eb637df4f2b4
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 18:41:56 +0500

    Question 15
    typescript

---
+++ 15/15a.ts
@@ -0,0 +1,5 @@
+let guestlist=["Sadia","Hina","Saba","Noreen"];
+let dontCome=guestlist[0];
+console.log(dontCome,"nahi aa sakhti");
+guestlist.splice(0,1,"Humaira");
+guestlist.forEach(guest=>console.log(`Assalam u Alaikum ${guest},would you like to dinnerwith me?`));
