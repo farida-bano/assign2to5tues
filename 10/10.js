b92d0f4b051b2c92fb7aedd8f953291651bdef17
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 18:36:58 +0500

    Question 10
    ts

---
+++ 10/10.js
@@ -0,0 +1,3 @@
+"use strict";
+let myName = "farida";
+console.log(`Hello ${myName}, would you like to learn some  html today?`);
---
+++ 10/10.ts
@@ -0,0 +1,2 @@
+let myName: string = "farida";
+console.log(`Hello ${myName}, would you like to learn some  html today?`);
