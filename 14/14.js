4ac6e1f426197ea459d4e58f3a664425d30d4189
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 18:40:58 +0500

    Question 14
    typescript

---
+++ 14/14.js
@@ -0,0 +1,3 @@
+"use strict";
+let guestlist = ["Farida ", "Rabia", "Sadia", "Ramsha"];
+guestlist.forEach(oneGuest => console.log(`Assalam u alaikum ${oneGuest} would you like to dinner with me?`));
---
+++ 14/14.ts
@@ -0,0 +1,2 @@
+let guestlist=["Farida ","Rabia","Sadia","Ramsha"];
+guestlist.forEach(oneGuest=>console.log(`Assalam u alaikum ${oneGuest} would you like to dinner with me?`));
