5f962ed50e93efde92e31569ba91df84ed260a5e
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 18:35:18 +0500

    Question 8
    typescript

---
+++ 8/8.js
@@ -0,0 +1,9 @@
+"use strict";
+//addition
+console.log(4 + 4);
+// subtraction
+console.log(12 - 4);
+//multiplication
+console.log(4 * 2);
+//Division
+console.log(16 / 2);
---
+++ 8/8.ts
@@ -0,0 +1,9 @@
+// 8:
+//addition
+console.log(4+4);
+// subtraction
+console.log(12-4);
+//multiplication
+console.log(4*2);
+//Division
+console.log(16/2);
