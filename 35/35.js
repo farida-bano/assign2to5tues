67672e774fbe6c3f4b061db8b33d3db4779a2d2e
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 19:05:25 +0500

    Question 35
    ts

---
+++ 35/35.js
@@ -0,0 +1,9 @@
+"use strict";
+// Array 
+const animals = ['Dog', 'Horse', 'Goat'];
+// Loop
+for (const animal of animals) {
+    console.log(`A ${animal.toLowerCase()} would make a great pet.`);
+}
+// Print outside the loop
+console.log(`Any of these animals would make a great pet!`);
---
+++ 35/35.ts
@@ -0,0 +1,10 @@
+// Array 
+const animals: string[] = ['Dog', 'Horse', 'Goat'];
+
+// Loop
+for (const animal of animals) {
+    console.log(`A ${animal.toLowerCase()} would make a great pet.`);
+}
+
+// Print outside the loop
+console.log(`Any of these animals would make a great pet!`);
