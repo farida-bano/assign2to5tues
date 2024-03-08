0cacf2bba6c9d3071c4f52c3bd6edccd7c8914e1
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 19:04:39 +0500

    Question 34
    ts

---
+++ 34/34.ts
@@ -0,0 +1,13 @@
+// Array 
+const pizzas: string[] = ['Malai Cheese', 'Chicken Tikka', 'Fajita'];
+
+// Loop
+for (const pizza of pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+
+// Additional sentences
+console.log("I can Eat Pizza daily");
+console.log("Pizza is Love");
+console.log("Cant Live Without Pizza");
+console.log(`I really love pizza!`);
