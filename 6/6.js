b70537cf6a4c19038e5747d571ca3cc0c721c62c
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 18:33:24 +0500

    Question 6
    typescript

---
+++ 6/6.js
@@ -0,0 +1,6 @@
+"use strict";
+//6.	Stripping Names: 
+let whitespaceName = "\n\t Farida Bano\t\n";
+console.log(whitespaceName);
+let withoutWhitespaceName = whitespaceName.trim();
+console.log(withoutWhitespaceName);
---
+++ 6/6.ts
@@ -0,0 +1,8 @@
+//6.	Stripping Names: 
+let whitespaceName= "\n\t Farida Bano\t\n";
+
+console.log(whitespaceName);
+
+let withoutWhitespaceName= whitespaceName.trim();
+
+console.log(withoutWhitespaceName);
