e683db5590441bc3adf16e9ba15f37028b31ec98
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 18:36:15 +0500

    Question 9
    ts

---
+++ 9/9.js
@@ -0,0 +1,5 @@
+"use strict";
+// Favorite Number: 
+let favName = 10;
+let message = "mine favorite number is:";
+console.log(message, favName);
---
+++ 9/9.ts
@@ -0,0 +1,5 @@
+// Favorite Number: 
+let favName =10;
+let message  ="mine favorite number is:";
+console.log (message,favName);
+
