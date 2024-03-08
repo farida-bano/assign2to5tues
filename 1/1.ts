2c77c154fea630e761a825f499c0d4d7106d4c1e
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 18:28:45 +0500

    question 1
    typescript

---
+++ 1/1.ts
@@ -0,0 +1,10 @@
+// Certainly! Here are some potential talking points or
+//good words that Governor Sindh "Kamran Sahab"might use when discussing 
+//the Governor Initiative for Artificial Intelligence, 
+//Web 3.0, and Metaverse visionary ,compassionate, charsmatic,
+//innovative,resilient,dedicated,inspiring,empathtic,admirable,
+//global leadership,collaborative,inclusive and transformational.
+//Assalam u alaikum:
+//Install Node.js, TypeScript and VS Code on your computer.
+
+console.log("Hello Pakistan,I am Farida");
