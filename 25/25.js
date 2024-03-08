2f535dbfbebe7920f5bff29665b6a4e1d5a37bd2
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 18:54:35 +0500

    Question 25
    ts

---
+++ 25/25.js
@@ -0,0 +1,11 @@
+"use strict";
+let alienColor = "purple";
+if (alienColor == "purple") {
+    console.log("Player earn 5+ points yahooooo!");
+}
+;
+// Second version of the program that fails the if test (no output):
+if (alienColor == "red") {
+    console.log("Player earn 5+ points yahooooo!");
+}
+;
---
+++ 25/25.ts
@@ -0,0 +1,11 @@
+let alienColor: string = "purple";
+
+if (alienColor == "purple") {
+    console.log("Player earn 5+ points yahooooo!");
+};
+
+// Second version of the program that fails the if test (no output):
+
+if (alienColor == "red") {
+    console.log("Player earn 5+ points yahooooo!");
+};
