a7eafa913cb3f3aa51f06a0972f840363a9537ed
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 19:06:05 +0500

    Question 36
    ts

---
+++ 36/36.js
@@ -0,0 +1,5 @@
+"use strict";
+function make_shirt(size, message) {
+    console.log(`You selected "${size}" size shirt with the "${message}" prints on it.`);
+}
+make_shirt("Medium", "Mirza Shirt");
---
+++ 36/36.ts
@@ -0,0 +1,5 @@
+function make_shirt(size: string, message: string) {
+    console.log(`You selected "${size}" size shirt with the "${message}" prints on it.`);
+}
+
+make_shirt("Medium","Mirza Shirt");
