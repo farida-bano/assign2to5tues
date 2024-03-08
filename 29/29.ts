8ce307e87e516e9ea4cc66e874b67f4915eefbfe
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 19:00:14 +0500

    Question 29
    ts

---
+++ 29/29.ts
@@ -0,0 +1,21 @@
+// Make an array of three favorite fruits
+const favorite_fruits: string[] = ['apple', 'banana', 'orange'];
+
+// Check for certain fruits in the array
+for (const fruit of favorite_fruits) {
+    if (fruit === 'apple') {
+        console.log("You really like apples!");
+    }
+    if (fruit === 'banana') {
+        console.log("You really like bananas!");
+    }
+    if (fruit === 'orange') {
+        console.log("You really like oranges!");
+    }
+    if (fruit === 'Strawberry') {
+        console.log("You really like Strawberry!");
+    } 
+    if (fruit === 'Cherry') {
+        console.log("You really like Cherry!");
+    } 
+}
