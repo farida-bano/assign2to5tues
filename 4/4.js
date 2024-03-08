2ce05a26cacf57e5f44e311b75e541da417449ee
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 18:31:39 +0500

    Question 4
    typescript

---
+++ 4/4.js
@@ -0,0 +1,5 @@
+"use strict";
+// 4.	Famous Quote: 
+console.log("Albert Einstein once said ,A person who never a mistake never tried anything new.");
+// Method 2 " ".
+console.log("Albert Einstein,\"once said ,A person who never a mistake never tried anything new.\"");
---
+++ 4/4.ts
@@ -0,0 +1,4 @@
+// 4.	Famous Quote: 
+console.log("Albert Einstein once said ,A person who never a mistake never tried anything new.");
+// Method 2 " ".
+console.log("Albert Einstein,\"once said ,A person who never a mistake never tried anything new.\"");
