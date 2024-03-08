370dfc7553337b5fb24fa6e8f6c8857bdb3efe87
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-08 18:53:03 +0500

    Question 23
    ts

---
+++ 23/23.ts
@@ -0,0 +1,46 @@
+let x = 5;
+let y = 10;
+let apple = "apple";
+let five = "five";
+let car = "car";
+let bike = "bike";
+
+// Test 1
+console.log("Is x == 5? I predict True.");
+console.log(x == 5);
+
+// Test 2
+console.log("Is y == 10? I predict True.");
+console.log(y == 10); 
+
+// Test 3
+console.log("Is x != 10? I predict True.");
+console.log(x != 10);
+
+// Test 4: Is y Greater than x?
+console.log("Is y > x? I predict True.");
+console.log(y > x); 
+
+// Test 5: Is x Less than y?
+console.log("Is x < y? I predict True.");
+console.log(x < y); 
+
+// Test 6: Is y Less than or Equal to x?
+console.log("Is y <= x? I predict False.");
+console.log(y <= x); 
+
+// Test 7:
+console.log("Is Apple == 'Banana'? I predict False.");
+console.log(apple == "banana");
+
+// Test 8:
+console.log("Is five == 'hundred'? I predict False.");
+console.log(five == "hundred");
+
+// Test 9:
+console.log("Is car == 'bike'? I predict False.")
+console.log(car == "bike");
+
+// Test 10:
+console.log("Is bike == 'car'? I predict False.")
+console.log(bike == "car");
